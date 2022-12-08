7dc4979fbc5f83186250cce169dacf69dbae00d3
Hisokula <113204321+Hisokula@users.noreply.github.com>
2022-12-08 10:55:44 +0200

    Add files via upload

---
+++ Lab6/inc/Func.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace esp
+{
+	void Outp(int mas[100][100]);
+	void Inp(int matrix[10000], int maas[100][100]);
+	void Process(int mas[100][100]);
+
+	int CountMatrix();
+	int ReadMatrix(int x);
+	bool Test(int mas[100][100]);
+	int Squares(int mas[100]);
+	int Minim(int mas[100]);
+}
