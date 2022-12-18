45759e65415df76ac8146b811e35d4134ecfa1d7
Hisokula <poymanova2004@mail.ru>
2022-12-18 17:23:53 +0200

    got it

--- Pr1/PR11.cpp
+++ Pr1/PR11.cpp
@@ -13,9 +13,13 @@ int main()
     esp::FlagSort(&str1, &str2);
 
     std::string sum = esp::Sum(str1, str2);
+    sum.erase(0, sum.find_first_not_of('0'));
 
     std::cout << "Sum: " << sum << std::endl;
 
-    std::cout << "Mult: " << esp::Mult(str1, str2) << std::endl;
+    std::string mult = esp::Mult(str1, str2);
+    mult.erase(0, mult.find_first_not_of('0'));
+
+    std::cout << "Mult: " << mult << std::endl;
 
 }
