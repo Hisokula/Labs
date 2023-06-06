c2e158f722b9ed4f6e0d865eec5125b7ac8e789e
Hisokula <poymanova2004@mail.ru>
2023-06-06 22:43:09 +0200

    again

--- 2nd semechka/Pr3/pr3.cpp
+++ 2nd semechka/Pr3/pr3.cpp
@@ -8,6 +8,7 @@ private:
     float m_alpha, m_beta, m_gamma = 0;
     float m_angle_A, m_angle_B = 0;
     int m_x, m_y = 0;
+    int errormark = 0;
 
 public:
 
@@ -26,11 +27,13 @@ public:
         if (L1 == 0)
         {
             std::cout << "Cannot be solved. Incorrect data." << std::endl;
+            errormark = 0;
         }
         else
         {
             m_L1 = L1;
             Solve(m_x, m_y);
+            errormark++;
         }
     }
 
@@ -39,19 +42,24 @@ public:
         if (L2 == 0)
         {
             std::cout << "Cannot be solved. Incorrect data." << std::endl;
+            errormark = 0;
         }
         else
         {
             m_L2 = L2;
             Solve(m_x, m_y);
+            errormark++;
         }
     }
 
     void Print()
     {
-        std::cout << "Link 1: " << m_L1 << "    Link 2: " << m_L2 << "    Link 3: " << m_L3 << " " << std::endl;
-        std::cout << "Alpha: " << m_alpha << "    Beta: " << m_beta << "    Gamma: " << m_gamma << std::endl;
-        std::cout << "A: " << m_angle_A << "    B: " << m_angle_B << std::endl;
+        if (errormark >= 3)
+        {
+            std::cout << "Link 1: " << m_L1 << "    Link 2: " << m_L2 << "    Link 3: " << m_L3 << " " << std::endl;
+            std::cout << "Alpha: " << m_alpha << "    Beta: " << m_beta << "    Gamma: " << m_gamma << std::endl;
+            std::cout << "A: " << m_angle_A << "    B: " << m_angle_B << std::endl;
+        }
     }
 
     void Solve(int x, int y)
@@ -59,12 +67,17 @@ public:
         if (abs((m_L2 * m_L2 + m_L3 * m_L3 - m_L1 * m_L1) / (2 * m_L2 * m_L3)) > 1)
         {
             std::cout << "Cannot be solved." << std::endl;
+            m_alpha = -1;
+            m_beta = -1;
+            m_gamma = -1;
+            errormark = 0;
         }
         else
         {
             m_alpha = acos((m_L2 * m_L2 + m_L3 * m_L3 - m_L1 * m_L1) / (2 * m_L2 * m_L3)) * (180 / acos(-1));
             m_beta = acos((m_L1 * m_L1 + m_L3 * m_L3 - m_L2 * m_L2) / (2 * m_L1 * m_L3)) * (180 / acos(-1));
             m_gamma = 180 - m_alpha - m_beta;
+            errormark+=3;
             Angles();
         }
     }
