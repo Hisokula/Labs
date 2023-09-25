8a78034c727aa7ad96f51b6172905d237bbe748a
Hisokula <113204321+Hisokula@users.noreply.github.com>
2023-09-25 23:59:52 +0300

    yeeeeeeee

--- 3rd semechka/ObjOrProg/Header.hpp
+++ 3rd semechka/ObjOrProg/Header.hpp
@@ -2,83 +2,83 @@
 #include<functional>
 #include<iostream>
 
-class Differentiation
+class Differentiation //базовый класс
 {
 protected:
-	double m_x0 = 0;
-	double m_dx = 0;
-	double m_accuracy = 0;
+	double m_x0 = 0; // точка
+	double m_dx = 0; // шаг
+	double m_accuracy = 0; // точность
 
-	virtual double DiffCalc(const std::function<double(double)>& f, double x0)
+	virtual double DiffCalc(const std::function<double(double)>& f, double x0) // абстрактная функция для взятия производной
 	{
 		return 0;
 	}
 
 public:
-	void Set_dx(double dx)
+	void Set_dx(double dx) // сеттер шага
 	{
 		dx = m_dx;
 	}
-	void Set_x0(double x0)
+	void Set_x0(double x0) // сеттер точки
 	{
 		x0 = m_x0;
 	}
-	double GetAccuracy()
+	double GetAccuracy() const // геттер точности
 	{
 		return m_accuracy;
 	}
-	double Get_dx()
+	double Get_dx() const // геттер шага
 	{
 		return m_dx;
 	}
-	Differentiation(double x0, double dx)
+	Differentiation(double x0, double dx) // конструктор объекта базового класса (используется в классах-наследниках)
 	{
 		m_x0 = x0;
 		m_dx = dx;
 	}
 };
 
-class LeftDiff : public Differentiation
+class LeftDiff : public Differentiation // класс для нахождения левой производной
 {
 public:
-	double DiffCalc(const std::function<double(double)>& f, double x0) override
+	double DiffCalc(const std::function<double(double)>& f, double x0) override // функция для нахождения производной класса-наследника
 	{
 		return static_cast<double>(f(m_x0) - f(m_x0 - m_dx)) / m_dx;
 	}
-	LeftDiff(double x0, double dx) : Differentiation(x0, dx)
+	LeftDiff(double x0, double dx) : Differentiation(x0, dx) // конструктор класса-наследника
 	{
 		m_x0 = x0;
 		m_dx = dx;
-		m_accuracy = m_dx;
+		m_accuracy = m_dx; //расчёт точности
 	}
 };
 
-class RightDiff : public Differentiation
+class RightDiff : public Differentiation // класс для нахождения правой производной
 {
 public:
-	double DiffCalc(const std::function<double(double)>& f, double x0) override
+	double DiffCalc(const std::function<double(double)>& f, double x0) override // функция для нахождения производной класса-наследника
 	{
 		return static_cast<double>(f(m_x0 + m_dx) - f(m_x0)) / m_dx;
 	}
-	RightDiff(double x0, double dx) : Differentiation(x0, dx)
+	RightDiff(double x0, double dx) : Differentiation(x0, dx) // конструктор класса-наследника
 	{
 		m_x0 = x0;
 		m_dx = dx;
-		m_accuracy = m_dx;
+		m_accuracy = m_dx; //расчёт точности
 	}
 };
 
-class CentralDiff : public Differentiation
+class CentralDiff : public Differentiation // класс для нахождения средней производной
 {
 public:
-	double DiffCalc(const std::function<double(double)>& f, double x0) override
+	double DiffCalc(const std::function<double(double)>& f, double x0) override // функция для нахождения производной класса-наследника
 	{
 		return static_cast<double>(f(m_x0 + m_dx) - f(m_x0 - m_dx)) / (2 * m_dx);
 	}
-	CentralDiff(double x0, double dx) : Differentiation(x0, dx)
+	CentralDiff(double x0, double dx) : Differentiation(x0, dx) // конструктор класса-наследника
 	{
 		m_x0 = x0;
 		m_dx = dx;
-		m_accuracy = m_dx * m_dx;
+		m_accuracy = m_dx * m_dx; //расчёт точности
 	}
 };
--- 3rd semechka/ObjOrProg/oop.cpp
+++ 3rd semechka/ObjOrProg/oop.cpp
@@ -4,14 +4,16 @@
 
 int main()
 {
-	std::function<double(double)> f = [](double x) { return x * x; };
+	std::function<double(double)> f = [](double x) { return x * x; }; // функция для проверки - x^2
+
+	//создание объектов наследных классов
 	LeftDiff LD(2, 0.001);
 	RightDiff RD(2, 0.001);
 	CentralDiff CD(2, 0.001);
 
-	std::cout << LD.DiffCalc(f, 2) << std::endl;
-	std::cout << RD.DiffCalc(f, 2) << std::endl;
-	std::cout << CD.DiffCalc(f, 2) << std::endl;
-
+	//вывод результата дифференцирования и точности
+	std::cout << "Left diff: " << LD.DiffCalc(f, 2) << " Accuracy: " << LD.GetAccuracy() << std::endl;
+	std::cout << "Right diff: " << RD.DiffCalc(f, 2) << " Accuracy: " << RD.GetAccuracy() << std::endl;
+	std::cout << "Central diff: " << CD.DiffCalc(f, 2) << " Accuracy: " << CD.GetAccuracy() << std::endl;
 
 }
