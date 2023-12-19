4ca168f6ee7e6e48f0a6fa6baa1b85a654dbbb36
Hisokula <poymanova2004@mail.ru>
2023-12-19 17:24:07 +0200

    1314

---
+++ 3rd semechka/Algorithms/AlgLab13.cpp
@@ -0,0 +1,132 @@
+﻿// хеш-таблица "с наложением"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+
+
+class HashElement
+{
+private:
+
+	int m_key;
+	std::string m_data;
+
+public:
+
+	HashElement(int key, std::string data) : m_key(key), m_data(data) {};
+
+	std::string getData() { return m_data; }
+
+	int getKey() { return m_key; }
+};
+
+
+class HashTable
+{
+private:
+
+	std::vector<HashElement> m_hash_table;
+	unsigned int start_size = 12;
+
+public:
+
+	HashTable()
+	{
+		for (int i = 0; i < start_size; ++i)
+		{
+			m_hash_table.push_back(HashElement(i, ""));
+		}
+	};
+
+	int GetStartSize() { return start_size; }
+
+	int HashFunc(const std::string& str)
+	{
+		int hash = 0;
+
+		for (int i = 0; i < str.size(); i++)
+		{
+			hash += (str[i] - '0');
+		}
+		return (hash / 23);
+	}
+
+	int GetSize() { return m_hash_table.size(); }
+
+	std::string GetData(const std::string& str)
+	{
+		int key = HashFunc(str);
+
+		for (int i = key; i < m_hash_table.size(); ++i)
+		{
+			if (m_hash_table[i].getData() == str)
+			{
+				return m_hash_table[i].getData();
+			}
+		}
+
+		return "No such data";
+	}
+
+	void HashTableInsert(const std::string data)
+	{
+		int key = HashFunc(data);
+
+		if (key >= m_hash_table.size())
+		{
+			m_hash_table.push_back(HashElement(key, data));
+		}
+		else
+		{
+			int i = key;
+			bool flag = false;
+
+			while (m_hash_table[i].getData() != "") { i++; }
+
+			if (m_hash_table.size() < i)
+			{
+				m_hash_table.push_back(HashElement(key, data));
+			}
+			else
+			{
+				m_hash_table[i] = HashElement(key, data);
+			}
+		}
+	}
+
+	HashElement operator[] (int key) { return m_hash_table[key]; }
+
+};
+
+int main()
+{
+	HashTable table;
+	std::string line;
+	int n = 1;
+
+	std::ifstream in("in.txt");
+	std::ofstream out;
+
+	if (in.is_open())
+	{
+		while (getline(in, line))
+		{
+			table.HashTableInsert(line);
+			std::cout << n << " - " << table.GetData(line) << std::endl;
+			n++;
+		}
+	}
+	in.close();
+
+	out.open("out.txt", std::ios::out | std::ios::trunc);
+	out << "Key | Index | Element\n";
+	int i = 0;
+	for (i; i < table.GetSize() - 1; i++)
+	{
+		out << i << "----------" << table[i].getKey() << "----------" << table[i].getData() << "\n";
+	}
+
+	out << i << "----------" << table[i].getKey() << "----------" << table[i].getData();
+	out.close();
+}
---
+++ 3rd semechka/Algorithms/AlgLab14.cpp
@@ -0,0 +1,95 @@
+﻿// хеш-таблица "со списками"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class HashTable 
+{
+private:
+
+    int m_size;
+    std::vector<std::vector<std::string>> m_table;
+
+public:
+
+    HashTable(int s) : m_size(s)
+    {
+        for (int i = 0; i < m_size; i++)
+        {
+            m_table.push_back({});
+        }
+    }
+
+    void fillWithLists(const std::string& str)
+    {
+        int ind = hash(str);
+        m_table[ind].push_back(str);
+    }
+
+    int hash(const std::string& s)
+    {
+        int hashValue = 0;
+        for (char ch : s)
+        {
+            hashValue += static_cast<int>(ch);
+        }
+        return hashValue % m_size;
+    }
+
+    int getSize() const
+    {
+        return m_size;
+    }
+
+    const std::vector<std::string>& operator[](int i) const
+    {
+        return m_table[i];
+    }
+};
+
+int main()
+{
+    std::ifstream in;
+    in.open("in.txt");
+
+    std::string el;
+    int size = 0;
+
+    while (in >> el) 
+    {
+        size++;
+    }
+
+    in.close();
+    in.open("in.txt");
+
+    std::string current;
+
+    HashTable table(size);
+
+    while (in >> current)
+    {
+        table.fillWithLists(current);
+    }
+
+    in.close();
+
+    std::ofstream out;
+    out.open("out.txt");
+
+    out << "Key | Value" << std::endl;
+
+    for (int i = 0; i < table.getSize(); i++) 
+    {
+        out << i << "\t";
+
+        for (const auto& value : table[i]) 
+        {
+            out << value << "  ";
+        }
+
+        out << "\n";
+    }
+
+    out.close();
+}
